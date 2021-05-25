0512a7de9c142b7b3633c61003b547c7e959e371
christopher-mendoza <chesmend@gmail.com>
2021-05-25 07:14:29 -0700

    Added basic room functionality

--- src/main/java/dev/mendoza/fixtures/Room.java
+++ src/main/java/dev/mendoza/fixtures/Room.java
@@ -1,5 +1,62 @@
 package dev.mendoza.fixtures;
 
 public class Room {
-
+	private Room[] exits;
+	private String name;
+	private String shortDesc;
+	private String longDesc;
+	public Room(String name, String shortDesc, String longDesc) {
+		super();
+		this.name = name;
+		this.shortDesc = shortDesc;
+		this.longDesc = longDesc;
+		this.exits = new Room[4];
+	}
+	
+	public Room[] getExits() {
+		return this.exits;
+	}
+	
+	public void setNorthExit(Room room) {
+		this.exits[0] = room;
+		room.exits[1] = this;
+	}
+	
+	public void setSouthExit(Room room) {
+		this.exits[1] = room;
+		room.exits[0] = this;
+	}
+	
+	public void setEastExit(Room room) {
+		this.exits[2] = room;
+		room.exits[3] = this;
+	}
+	
+	public void setWestExit(Room room) {
+		this.exits[3] = room;
+		room.exits[2] = this;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getShortDesc() {
+		return shortDesc;
+	}
+	public void setShortDesc(String shortDesc) {
+		this.shortDesc = shortDesc;
+	}
+	
+	public String getLongDesc() {
+		return longDesc;
+	}
+	public void setLongDesc(String longDesc) {
+		this.longDesc = longDesc;
+	}
+	
+	
 }
--- src/main/java/dev/mendoza/game/Main.java
+++ src/main/java/dev/mendoza/game/Main.java
@@ -1,10 +1,20 @@
 package dev.mendoza.game;
 
+import dev.mendoza.fixtures.Room;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Room test = new Room("room1", "this is a room", "this is a longer room description");
+		Room test2 = new Room("room2", "this is another room", "this is another longer room description");
+		
+		test.setNorthExit(test2);
+		Room[] arrayHolder = test.getExits();
+		for(int i = 0; i < test.getExits().length; i++) {
+			System.out.println(i);
+			System.out.println(arrayHolder[i].getName());
+		}
 	}
 
 }
