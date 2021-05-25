234b78ddabf5a98111f5f43f519e82a26a1288a2
christopher-mendoza <chesmend@gmail.com>
2021-05-25 08:26:31 -0700

    Added basic fixture functionality

--- src/main/java/dev/mendoza/fixtures/Fixture.java
+++ src/main/java/dev/mendoza/fixtures/Fixture.java
@@ -1,5 +1,34 @@
 package dev.mendoza.fixtures;
 
 public class Fixture {
-
+	private String name;
+	private String shortDesc;
+	private String longDesc;
+	
+	public Fixture(String name, String shortDesc, String longDesc) {
+		super();
+		this.name = name;
+		this.shortDesc = shortDesc;
+		this.longDesc = longDesc;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getShortDesc() {
+		return shortDesc;
+	}
+	public void setShortDesc(String shortDesc) {
+		this.shortDesc = shortDesc;
+	}
+	public String getLongDesc() {
+		return longDesc;
+	}
+	public void setLongDesc(String longDesc) {
+		this.longDesc = longDesc;
+	}
+	
 }
